c1dfb95bd9e966fb920d4e821777202d67d39d79
Naman <70206828+naman2398@users.noreply.github.com>
2023-12-08 02:17:18 +0530

    adding custom config and additional language support

--- ocr_module/text_recognition.py
+++ ocr_module/text_recognition.py
@@ -2,12 +2,14 @@ import pytesseract
 from PIL import Image
 import cv2
 
-def perform_ocr(image):
-    
+def perform_ocr(image, lang='eng'):
+    # Perform OCR using Tesseract with specified language
+    custom_config = r'--oem 3 --psm 6 outputbase digits'  # Tesseract configuration options
+
     # Convert OpenCV image to PIL Image for compatibility with pytesseract
     pil_image = Image.fromarray(image)
 
     # Perform OCR using Tesseract
-    text = pytesseract.image_to_string(pil_image)
+    text = pytesseract.image_to_string(pil_image, lang=lang, config=custom_config)
 
     return text
